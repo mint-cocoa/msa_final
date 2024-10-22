5279951c4fb93ebf0a0d3ebd286bd1a10151e8b2
jinhoo bae <61346064+mint-cocoa@users.noreply.github.com>
2024-10-22 14:43:50 +0900

    update bug

--- external-services/user_service/app/database.py
+++ external-services/user_service/app/database.py
@@ -3,9 +3,6 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
-
-
-
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://root:example@mongodb:27017/users?authSource=admin")
