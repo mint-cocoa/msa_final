4f8439b50c4328db159354a7103febcbada6079c
jinhoo bae <61346064+mint-cocoa@users.noreply.github.com>
2024-10-25 14:04:38 +0900

    update mq

--- external-services/user_service/app/main.py
+++ external-services/user_service/app/main.py
@@ -1,7 +1,7 @@
 # user_service/app/main.py
 from fastapi import FastAPI
 from .routes import router as user_router
-
+from .database import Database
 
 app = FastAPI(
     title="User Service",
@@ -14,4 +14,12 @@ app = FastAPI(
 def read_root():
     return {"message": "Welcome to the User Service"}
 
+@app.on_event("startup")
+async def startup_db_client():
+    await Database.connect_db()
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    await Database.close_db()
+  
 app.include_router(user_router, prefix="/api")  # '/api' 대신 '/users'를 사용
